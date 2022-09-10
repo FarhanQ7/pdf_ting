975410313ac1ab9c8f8f5f373e25b6503d54f4af
Farhan <farhan.q@live.com>
2022-09-10 04:12:35 +0000

    second cmmit App

--- pdf_ting/urls.py
+++ pdf_ting/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from pdf_app.views import Home
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),path('', Home.as_view(), name='home'),
 ]
