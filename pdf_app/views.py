14187f61559d9ed5320e148e1f09dbcf9b7ecaab
Farhan <farhan.q@live.com>
2022-09-12 01:28:41 +0000

    added views and forms

--- pdf_app/views.py
+++ pdf_app/views.py
@@ -1,5 +1,14 @@
 from django.views.generic import TemplateView
 
 class Home(TemplateView):
-    permission_classes = (permissions.AllowAny,)
+
     template_name = "home.html"
+    def upload_file(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            handle_uploaded_file(request.FILES['file'])
+            return HttpResponseRedirect('/success/url/')
+    else:
+        form = UploadFileForm()
+    return render(request, 'upload.html', {'form': form})
