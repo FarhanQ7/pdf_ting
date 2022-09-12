5f9852e1d23dcdc60217dcf9acdb4bc144e1e01e
Farhan <farhan.q@live.com>
2022-09-12 01:38:36 +0000

    indented some things

--- pdf_app/views.py
+++ pdf_app/views.py
@@ -4,11 +4,11 @@ class Home(TemplateView):
 
     template_name = "home.html"
     def upload_file(request):
-    if request.method == 'POST':
-        form = UploadFileForm(request.POST, request.FILES)
-        if form.is_valid():
-            handle_uploaded_file(request.FILES['file'])
-            return HttpResponseRedirect('/success/url/')
-    else:
-        form = UploadFileForm()
-    return render(request, 'upload.html', {'form': form})
+        if request.method == 'POST':
+            form = UploadFileForm(request.POST, request.FILES)
+            if form.is_valid():
+                handle_uploaded_file(request.FILES['file'])
+                return HttpResponseRedirect('/success/url/')
+        else:
+            form = UploadFileForm()
+        return render(request, 'upload.html', {'form': form})
